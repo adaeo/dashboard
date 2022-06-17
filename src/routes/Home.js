d09064adb94cb3c6f4388bdd1e692d04ebedbe4e
adaeo <wangj9507@gmail.com>
2022-06-17 16:42:48 +1000

    Add file upload method

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button, Container } from "reactstrap";
+import { Button, Container, Input } from "reactstrap";
 
 export default function Home(props) {
   const url = "https://adaeo-server.herokuapp.com/devtest";
@@ -20,6 +20,32 @@ export default function Home(props) {
     }
   }
 
+  const [selectedFile, setSelectedFile] = useState();
+  const [isSelected, setIsSelected] = useState(false);
+
+  const changeHandler = (event) => {
+    setSelectedFile(event.target.files[0]);
+    setIsSelected(true);
+  };
+
+  const handleSubmission = () => {
+    const formData = new FormData();
+
+    formData.append("file", selectedFile);
+
+    fetch("https://adaeo-server.herokuapp.com/devtest/upload", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log("Success:", result);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
   return (
     <main>
       <Container>
@@ -31,6 +57,27 @@ export default function Home(props) {
         >
           Press Me
         </Button>
+      </Container>
+      <Container>
+        <div>
+          <Input type="file" name="file" onChange={changeHandler} />
+          {isSelected ? (
+            <div>
+              <p>Filename: {selectedFile.name}</p>
+              <p>Filetype: {selectedFile.type}</p>
+              <p>Size in bytes: {selectedFile.size}</p>
+              <p>
+                lastModifiedDate:{" "}
+                {selectedFile.lastModifiedDate.toLocaleDateString()}
+              </p>
+            </div>
+          ) : (
+            <p>Select a file to show details</p>
+          )}
+          <div>
+            <Button onClick={handleSubmission}>Submit</Button>
+          </div>
+        </div>
       </Container>
     </main>
   );
