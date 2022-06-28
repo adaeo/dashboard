d7d2a4652907c8c26da9f283f267e2c3dc4f03d8
adaeo <wangj9507@gmail.com>
2022-06-28 17:19:42 +1000

    small cleanup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, Routes, Route, HashRouter } from "react-router-dom";
+import { Link, Routes, Route } from "react-router-dom";
 
 // styles import
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -12,9 +12,9 @@ import DevTest from "./routes/DevTest";
 function App() {
   return (
     <div>
-      <Link to="/home">Home</Link>
-      <Link to="/devtest">DevTest</Link>
       <Routes>
+        <Link to="/">Home</Link>
+        <Link to="/devtest">DevTest</Link>
         <Route path="/" element={<Home />} />
         <Route path="/devtest" element={<DevTest />} />
       </Routes>
